0a00ef62a20d53098fa21ba0c393821aca2f466e
AVENGA\bogdan.grebeniuk <bogdan.grebeniuk@avenga.com>
2023-04-11 21:55:43 +0300

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,3 @@
 import '../node_modules/bootstrap-styles/css/bootstrap.css';
 import './index.scss';
-//import './web-blocks/compiler';
-// import './web-blocks/index1';
-// import './web-blocks/index3';
 import './demo';
